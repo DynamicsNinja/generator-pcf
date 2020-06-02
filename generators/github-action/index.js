de54c6db58792ee5ced534ea1390e607e6331a44
Power-Maverick <danish.naglekar@hotmail.com>
2020-06-02 08:12:47 -0400

    Prettified file + added NPM cmd for start watch 👌

--- generators/github-action/index.js
+++ generators/github-action/index.js
@@ -1,30 +1,35 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const utils = require('../utils');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const utils = require("../utils");
 
 module.exports = class extends Generator {
-    constructor(args, opts) {
-        super(args, opts);
-    }
+  // eslint-disable-next-line no-useless-constructor
+  constructor(args, opts) {
+    super(args, opts);
+  }
 
-    prompting() {
-        utils.greeting(this);
-    }
+  prompting() {
+    utils.greeting(this);
+  }
 
-    writing() {
-        this.fs.copyTpl(
-            this.templatePath('_build.yml'),
-            this.destinationPath(`.github/workflows/build.yml`)
-        );
-    }
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath("_build.yml"),
+      this.destinationPath(`.github/workflows/build.yml`)
+    );
+  }
 
-    this
-
-    end() {
-        this.log(chalk.yellow('\nWARNING\n'));
-        this.log(`GitHub Action will not work until you create GITHUB_TOKEN secret with value of your personal access token in Settings->Secrets inside the repository.`);
-        this.log(`You can find step by step guide how to create personal access token here:`);
-        this.log(`https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line#creating-a-token`);
-    }
-}
+  end() {
+    this.log(chalk.yellow("\nWARNING\n"));
+    this.log(
+      `GitHub Action will not work until you create GITHUB_TOKEN secret with value of your personal access token in Settings->Secrets inside the repository.`
+    );
+    this.log(
+      `You can find step by step guide how to create personal access token here:`
+    );
+    this.log(
+      `https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line#creating-a-token`
+    );
+  }
+};
--- generators/utils.js
+++ generators/utils.js
@@ -1,47 +1,57 @@
-const yosay = require('yosay');
-const chalk = require('chalk');
+const yosay = require("yosay");
+const chalk = require("chalk");
 
 module.exports = {
-    checkPrerequisites,
-    greeting
-}
+  checkPrerequisites,
+  greeting
+};
 
 function checkPrerequisites(generator, skipMsBuild) {
-    var paths = process.env.path.split(";");
+  var paths = process.env.path.split(";");
 
-    var msbuildFound = skipMsBuild;
-    var pacFound = false;
+  var msbuildFound = skipMsBuild;
+  var pacFound = false;
 
-    paths.forEach(path => {
-        if (path.indexOf("PowerAppsCLI") != -1) {
-            pacFound = true;
-        } else if (path.indexOf("MSBuild") != -1) {
-            msbuildFound = true;
-        }
-    });
-
-    if (pacFound && msbuildFound) {
-        generator.log(`Checking prerequisites ${chalk.green("OK")}\n`);
-        return;
+  paths.forEach(path => {
+    if (path.indexOf("PowerAppsCLI") !== -1) {
+      pacFound = true;
+    } else if (path.indexOf("MSBuild") !== -1) {
+      msbuildFound = true;
     }
+  });
 
-    generator.log(`Checking prerequisites ${chalk.red("NOK")}`);
+  if (pacFound && msbuildFound) {
+    generator.log(`Checking prerequisites ${chalk.green("OK")}\n`);
+    return;
+  }
 
-    if (!msbuildFound) {
-        generator.log(chalk.yellow('\nWARNING'));
-        generator.log("MSBuild not found in your path variable. Please add it to proceed.\nIt's usually located at C:\\Users\\<YOUR_USER>\\AppData\\Local\\Microsoft\\PowerAppsCLI\\");
-    }
+  generator.log(`Checking prerequisites ${chalk.red("NOK")}`);
 
-    if (!pacFound) {
-        generator.log(chalk.yellow('\nWARNING'));
-        generator.log(`Power Apps CLI not found in your path variable. Please add it to proceed.\nYou can download it from: https://aka.ms/PowerAppsCLI`);
-    }
+  if (!msbuildFound) {
+    generator.log(chalk.yellow("\nWARNING"));
+    generator.log(
+      "MSBuild not found in your path variable. Please add it to proceed.\nIt's usually located at C:\\Users\\<YOUR_USER>\\AppData\\Local\\Microsoft\\PowerAppsCLI\\"
+    );
+  }
 
-    process.exit(-1);
+  if (!pacFound) {
+    generator.log(chalk.yellow("\nWARNING"));
+    generator.log(
+      `Power Apps CLI not found in your path variable. Please add it to proceed.\nYou can download it from: https://aka.ms/PowerAppsCLI`
+    );
+  }
+
+  process.exit(-1);
 }
 
 function greeting(generator) {
-    generator.log(
-        yosay(`Yo! Time to code this amazing ${chalk.hex('#752875').bold('PCF')} control\nthat came to your mind!\n${chalk.hex('#0066FF').bold(' #ProCodeNoCodeUnite')}`)
-    );
+  generator.log(
+    yosay(
+      `Yo! Time to code this amazing ${chalk
+        .hex("#752875")
+        .bold("PCF")} control\nthat came to your mind!\n${chalk
+        .hex("#0066FF")
+        .bold(" #ProCodeNoCodeUnite")}`
+    )
+  );
 }
